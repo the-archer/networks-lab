cfd73d373b5bdf2b9370ce7bca90c79a36de5d38
Simrat Singh Chhabra <simratsinghchhabra@gmail.com>
2014-02-19 16:10:42 +0530

    Added getreqfromclient function

--- proxy.cpp
+++ proxy.cpp
@@ -19,6 +19,11 @@ using namespace std;
 
 #define BACKLOG 3
 
+
+int get_req_from_client(int new_fd, char *buf, int buf_size);
+int parse_req(char *buf, int max);
+
+
 void sigchld_handler(int s)
 {
 	while(waitpid(-1, NULL, WNOHANG) > 0);
@@ -33,6 +38,7 @@ void *get_in_addr(struct sockaddr *sa)
 
 	return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
+
 
 
 
@@ -51,12 +57,13 @@ int main(int argc, char *argv[])
 	int yes=1;
 	char s[INET6_ADDRSTRLEN];
 	int rv;
-
+	int req_stat;
+	
 	memset(&hints, 0, sizeof hints);
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE; // use my IP
-
+	int byte_count;
 	if ((rv = getaddrinfo(NULL, argv[1], &hints, &servinfo)) != 0) {
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
 		return 1;
@@ -109,31 +116,60 @@ int main(int argc, char *argv[])
 	while(1) {  
 
 		sin_size = sizeof their_addr;
-		cout << "before accept" << endl;
+		//cout << "before accept" << endl;
 		new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-		cout << "after accept" << endl;
+		//cout << "after accept" << endl;
 		if (new_fd == -1) {
 			perror("accept");
 			cout << "Here sir" << endl;
 			//continue;
 		}
-
+		char buf[512];
 		inet_ntop(their_addr.ss_family,
 			get_in_addr((struct sockaddr *)&their_addr),
 			s, sizeof s);
-
 		printf("server: got connection from %s\n", s);
+		//printf("%d\n", sizeof buf);
+		byte_count=get_req_from_client(new_fd, buf, sizeof buf);
+		
+		//
+
+		//send_req_to_server()
+
+		//rec_reply_from_server()
+
+		//send_reply_to_client()
+
+		
+
+		
+		printf("recv()'d %d bytes of data in buf\n", byte_count);
+		int k=0;
+		while(k<byte_count)
+		{
+			printf("%c", buf[k]);
+			k++;
+		}
+		printf("\n");
+
+		int pr = parse_req(buf, byte_count);
+		
+
+		close(new_fd);
+		/*printf("server: got connection from %s\n", s);
 		if (send(new_fd, "Hello, world!", 13, 0) == -1)
 				perror("send");
+		*/	
+			//char* temp;
 			
-			char* temp;
-			scanf("%s", temp);
+
+			/*scanf("%s", temp);
 			if (send(new_fd, temp, strlen(temp), 0) == -1)
 				perror("send");
 			
 
 			close(new_fd);
-			//exit(0);
+			*///exit(0);
 
 
 		/*if (!fork()) { // this is the child process
@@ -151,5 +187,53 @@ int main(int argc, char *argv[])
 
 
 	return 0;
+
+}
+
+int get_req_from_client(int new_fd, char *buf, int buf_size)
+{
+	bool end=false;
+	
+	int cur_count=0;
+	//char buf[512];
+	while(!end)
+	{
+
+
+	int byte_count = recv(new_fd, buf+cur_count, buf_size, 0);
+	
+	//printf("recv()'d %d bytes of data in buf\n", byte_count);
+		int k=0;
+		while(k<byte_count)
+		{
+			if(buf[k]=='\r' && buf[k+1]=='\n' && buf[k+2]=='\r' && buf[k+3]=='\n')
+			{
+				//printf("here\n");
+				end=true;
+				break;
+
+			}	
+			//printf("%c", buf[k]);
+			k++;
+
+		}
+		cur_count+=byte_count;
+		printf("\n");
+	}	
+	return cur_count;
+
+
+}
+
+
+int parse_req(char *buf, int max)
+{
+	if(strcmp("GET", buf)==0)
+	{
+		printf("Matched\n");
+	}
+	return 0;
+
+
 
 }
